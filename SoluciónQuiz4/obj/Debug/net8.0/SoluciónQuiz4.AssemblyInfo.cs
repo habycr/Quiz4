7817af12a563ce234a81d9b23d7e6eef284c9439
Fabio-Ruiz <132989230+Fabio-Ruiz@users.noreply.github.com>
2025-05-28 19:29:23 -0600

    acomodo

--- SoluciónQuiz4/obj/Debug/net8.0/SoluciónQuiz4.AssemblyInfo.cs
+++ SoluciónQuiz4/obj/Debug/net8.0/SoluciónQuiz4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoluciónQuiz4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+188755d5aeba02e87ce714bc8c64661790880067")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e05675138113a240808c7f7226217647d3b8cf4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoluciónQuiz4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoluciónQuiz4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
